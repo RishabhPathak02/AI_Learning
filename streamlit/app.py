c79ecc6443f047b3165f100b5a949a2b6a3fe3ef
Rishabh Pathak <rishabh06pathak@gmail.com>
2026-05-17 08:47:18 +0000

    feat : added basic of python and streamlit

---
+++ streamlit/app.py
@@ -0,0 +1,36 @@
+import streamlit as st 
+import pandas as pd 
+import numpy as np
+
+st.title("My streamlit app")
+st.write("This is write using steamlit")
+
+df = pd.DataFrame({
+    'first column' : [1,2,3,4],
+    'second column' : [10,20,30,40]
+})
+
+st.write(df)
+
+chart_data = pd.DataFrame(
+    np.random.randn(20,3),
+    columns=['a','b','c']
+)
+st.line_chart(chart_data)
+
+import streamlit as st
+import time
+
+'Starting a long computation...'
+
+# Add a placeholder
+latest_iteration = st.empty()
+bar = st.progress(0)
+
+for i in range(100):
+  # Update the progress bar with each iteration.
+  latest_iteration.text(f'Iteration {i+1}')
+  bar.progress(i + 1)
+  time.sleep(0.5)
+
+'...and now we\'re done!'
